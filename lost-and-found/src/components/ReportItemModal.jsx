20589b25e4e31ffadebf94123d2e3dda5295fddb
ybopeya <ybopeya2@illinois.edu>
2026-05-04 17:19:50 -0500

    Update ReportItemModal.jsx

--- lost-and-found/src/components/ReportItemModal.jsx
+++ lost-and-found/src/components/ReportItemModal.jsx
@@ -18,11 +18,6 @@ function ReportItemModal({ isOpen, onClose }) {
     const { data: itemTypeData, error: itemTypeError } = await supabase
       .from('item_types')
       .select('id, name');
- 
-console.log("itemTypeData:", itemTypeData);
-console.log("itemTypeError:", itemTypeError);
-console.log("locationData:", locationData);
-console.log("locationError:", locationError);
     
     const { data: locationData, error: locationError } = await supabase
       .from('locations')
