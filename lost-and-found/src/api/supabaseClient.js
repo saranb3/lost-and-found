b21a62282dc7c698fc15e87bbf0bf17af153c1d5
ybopeya <ybopeya2@illinois.edu>
2026-05-04 16:58:00 -0500

    adding console logs to track supabase issue

--- lost-and-found/src/api/supabaseClient.js
+++ lost-and-found/src/api/supabaseClient.js
@@ -3,4 +3,9 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
+console.log("URL:", supabaseUrl);
+console.log("KEY EXISTS:", !!supabaseAnonKey);
+
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+console.log("SUPABASE:", supabase);
